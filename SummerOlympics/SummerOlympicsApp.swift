8cc79c23601cdd69f1757f5e0b119972e98e0d66
Stacie Rivera <stacierivera96@gmail.com>
2026-04-17 12:59:05 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by RIVERA, STACIE on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
